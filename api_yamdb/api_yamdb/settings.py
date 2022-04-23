2800689ee591ea8eb48ad4dba55e876b81c797d9
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-04-23 15:51:38 +0300

    изм STATICFILES_DIRS

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -114,8 +114,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = ('redoc', '/app/templates/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    (os.path.join(BASE_DIR, 'static/')),
+    ('redoc', '/app/templates/')
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
